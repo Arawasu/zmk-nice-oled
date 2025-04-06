444d02ddc7bc23c399072740a61382bacda385eb
null <null>
2025-04-06 21:52:46 +0200

    forgot function def lol

--- boards/shields/nice_oled/nice_epaper/widgets/wpm.c
+++ boards/shields/nice_oled/nice_epaper/widgets/wpm.c
@@ -123,16 +123,18 @@ static void draw_label(lv_obj_t *canvas, const struct status_state *state) {
     lv_canvas_draw_text(canvas, 26, 103, 42, &label_dsc_wpm, wpm_text);
 }
 
-#if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_WPM) && IS_ENABLED(CONFIG_NICE_OLED_WIDGET_WPM_LUNA)
-    // Show Luna only – skip everything else
-    draw_label(canvas, state);
-#elif IS_ENABLED(CONFIG_NICE_OLED_WIDGET_WPM)
-    // Show gauge/needle/graph if Luna isn't enabled
-    draw_gauge(canvas, state);
-    draw_needle(canvas, state);
-    draw_grid(canvas);
-    draw_graph(canvas, state);
-    draw_label(canvas, state);
-#else
-    // No WPM at all
-#endif
+void draw_wpm_status(lv_obj_t *canvas, const struct status_state *state) {
+    #if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_WPM) && IS_ENABLED(CONFIG_NICE_OLED_WIDGET_WPM_LUNA)
+        // Show Luna only – skip everything else
+        draw_label(canvas, state);
+    #elif IS_ENABLED(CONFIG_NICE_OLED_WIDGET_WPM)
+        // Show gauge/needle/graph if Luna isn't enabled
+        draw_gauge(canvas, state);
+        draw_needle(canvas, state);
+        draw_grid(canvas);
+        draw_graph(canvas, state);
+        draw_label(canvas, state);
+    #else
+        // No WPM at all
+    #endif
+}
