e239b8695cb8bcc35d23f4d5c52432da3697b832
null <null>
2025-04-07 17:26:07 +0200

    moved luna

--- boards/shields/nice_oled/nice_epaper/widgets/screen.c
+++ boards/shields/nice_oled/nice_epaper/widgets/screen.c
@@ -259,7 +259,7 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
 
 #if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_WPM)
     zmk_widget_luna_init(&luna_widget, canvas);
-    lv_obj_align(zmk_widget_luna_obj(&luna_widget), LV_ALIGN_TOP_LEFT, 50, 22);
+    lv_obj_align(zmk_widget_luna_obj(&luna_widget), LV_ALIGN_TOP_LEFT, 40, 22);
 #endif
 
 // #if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_HID_INDICATORS)
