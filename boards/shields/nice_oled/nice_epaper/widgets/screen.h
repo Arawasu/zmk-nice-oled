7490081138380e0d91512e54511fc4eaa3877844
null <null>
2025-04-07 02:04:20 +0200

    removed extra status state struct

--- boards/shields/nice_oled/nice_epaper/widgets/screen.h
+++ boards/shields/nice_oled/nice_epaper/widgets/screen.h
@@ -14,17 +14,3 @@ struct zmk_widget_screen {
 int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent);
 lv_obj_t *zmk_widget_screen_obj(struct zmk_widget_screen *widget);
 
-struct status_state {
-    uint8_t battery;
-    bool charging;
-
-    struct zmk_endpoint_instance selected_endpoint;
-    int active_profile_index;
-    bool active_profile_connected;
-    bool active_profile_bonded;
-
-    const char *layer_label;
-    uint8_t layer_index;
-
-    uint8_t wpm[10];
-};
